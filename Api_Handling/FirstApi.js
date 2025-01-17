ea31c158d0d2a7f34bd6b5ab247cc67bb237929d
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-01-17 23:05:59 +0530

    Updated files

---
+++ Api_Handling/FirstApi.js
@@ -0,0 +1,5 @@
+// It has 4 method get, put, post and delete
+// get() to get data from database
+// put() to update data in database
+// post() to insert data in database
+// delete() to delete data from database
