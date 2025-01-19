8a37d0705210eb3de282872fee6e54391eaa63a6
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-01-19 10:49:47 +0530

    Updated Api.js

--- Api_Handling/Api.js
+++ Api_Handling/Api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoconnection = require('../MongoDB/mongodb');
 const app = express();
 
+// Used to get body form request
+app.use(express.json());
+
 app.get('/',async(req,resp)=>{
     let data = await mongoconnection();
     data = await data.find().toArray();
@@ -9,6 +12,16 @@ app.get('/',async(req,resp)=>{
 
 
 });
-app.listen(3000);
 
+
+app.post('/',async(req,resp)=>{
+    let data = await mongoconnection();
+    let result = await data.insertOne(req.body);
+    resp.send(result);
+
+})
+
+app.listen(3100);
+
+// To read data form the Api we use get method
 // We cannot pass body in get method
---
+++ Api_Handling/postApi.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const dbconnect = require('../MongoDB/mongodb');
+// To save new data in database through api than will use post method 
+
