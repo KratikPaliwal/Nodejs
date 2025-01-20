bbd9fd9fef8cbc1504e47d842beec4c78108fb9f
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-01-20 21:30:00 +0530

    Updated code

--- Api_Handling/Mongoose.js
+++ Api_Handling/Mongoose.js
@@ -1,3 +1,18 @@
 // Schema Validate the entry in database if we have entered only 5 entries than user cannot enter more than 5 entries
 
 // Model connect mongodb with nodejs 
+
+const mongoose = require('mongoose');
+const main  = async()=>{
+    await mongoose.connect('mongodb://localhost:27017/E-comm');
+    const ProductSchema = new mongoose.Schema({
+        name:String,
+        price:Number
+    });
+    const productModel = mongoose.model('Products',ProductSchema);
+    let data = new productModel({name : "m4", price : 1000});
+    let result = await data.save();
+    console.log(result);
+
+}
+main();
