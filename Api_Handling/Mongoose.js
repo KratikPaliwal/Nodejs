bfa0d08fb68eb269f6e2eed39aa62ae040a1f761
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-01-21 07:21:34 +0530

    Added files

--- Api_Handling/Mongoose.js
+++ Api_Handling/Mongoose.js
@@ -2,17 +2,58 @@
 
 // Model connect mongodb with nodejs 
 
-const mongoose = require('mongoose');
-const main  = async()=>{
-    await mongoose.connect('mongodb://localhost:27017/E-comm');
-    const ProductSchema = new mongoose.Schema({
-        name:String,
-        price:Number
-    });
-    const productModel = mongoose.model('Products',ProductSchema);
-    let data = new productModel({name : "m4", price : 1000});
-    let result = await data.save();
+
+// To save data in db
+const  mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/E-comm');
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    Brands:String,
+    Category:String
+},
+{
+    collection:'Products'
+}
+);
+
+const main = async ()=>{
+    const Product = mongoose.models.Products || mongoose.model('Products', productSchema);
+
+    let data = new Product({name:"qw00",price:1001,Brands:'QT',Category:'QWW'});
+    const result = await data.save();
     console.log(result);
+
 
 }
 main();
+
+
+// To update data in db;
+
+const UpdateData = async ()=>{
+    const Product = mongoose.models.Products || mongoose.model('Products', productSchema);
+    let data = await Product.updateMany(
+        {name: 'qw00'},
+        {$set:{price:1111}}
+    )
+    console.log(data);
+
+}
+
+UpdateData();
+
+const deleteData = async()=>{
+    const Product = mongoose.model.Products || mongoose.model('Products',productSchema);
+    let data = await Product.deleteOne({name :'Apple'});
+    console.log(data);
+}
+
+// deleteData();
+const findInDb = async()=>{
+    const Product = mongoose.model.Products || mongoose.model('Products',productSchema);
+    let data = await Product.find({name: 'qw00'});
+    console.log(data);
+}
+
+findInDb();
